e4d5b6ecb56b07e83fd4c443b549aaa14f304292
Luis Valgoi <luisvalgoi@gmail.com>
2023-06-08 19:33:11 -0300

    chore: adds public

---
+++ build/precache-manifest.9ab67e680decfcc02d6e4c10712d892e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f401e5e5f6df3f1f168a69d1800773f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "731bc8d8aaa89701d51d",
+    "url": "/static/css/main.e3df669c.chunk.css"
+  },
+  {
+    "revision": "227895f4350e4725e1d0",
+    "url": "/static/js/2.4df46b50.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.4df46b50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "731bc8d8aaa89701d51d",
+    "url": "/static/js/main.9c793a46.chunk.js"
+  },
+  {
+    "revision": "250e79f0bc1356899259",
+    "url": "/static/js/runtime-main.2e304d47.js"
+  }
+]);
